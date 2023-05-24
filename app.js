5f24bdfbdc861bcea88bc93137bb472cfc59637b
Anthony <semprini.anthony@gmail.com>
2023-05-24 13:19:24 +0200

    cree fonction aleatoire de joueurOrdi

--- app.js
+++ app.js
@@ -12,5 +12,61 @@ let nextBtn = document.getElementById("next");
 //logique
 const jouerManche = (e) => {
     let choix = e.target.closest(".btn-joueur");//sert a recup tj la div
+
+    btnJoueur.forEach((btn) =>{
+    btn.classList.add("desactivated");
+    btn.removeEventListener("click", jouerManche);
+    });
+
+    choix.classList.remove("desctivated");
+    choix.classList.add("active");
+
+    let choixJoueur = choix.id;
+
+    let choixOrdi = faireChoixOrdinateur();
+
+    verifierGagnant(choixJoueur, choixOrdi);
 };
+const PIERRE = "pierre";
+const FEUILLE = "feuille";
+const CISEAUX = "ciseaux";
+
+const faireChoixOrdinateur = () => {
+    //0 = pierre
+    // 1 = feuille
+    // 2 = ciseaux
+
+    let nbAleatoire = Math.floor(Math.random() *3);
+    switch(nbAleatoire){
+        case 0:
+            opierre.classList.add("active");
+            return PIERRE;
+
+        case 1:
+            ofeuille.classList.add("active");
+            return FEUILLE;
+
+        case 2:
+            ociseaux.classList$.add("active");
+            return CISEAUX;
+ }
+};
+
+const verifierGagnant = (choixJoueur, choixOrdi) => {
+    if(choixJoueur == choixOrdi){
+        message.textContent = "Egalité !";
+        return
+    }
+
+    if(choixJoueur == PIERRE){
+        if(choixOrdi == FEUILLE){
+            return victoirOrdinateur();
+        }
+    }
+};
+
+const victoireOrdinateur = () => {
+    
+}
+
 btnJoueur.forEach((btn) => btn.addEventListener("click", jouerManche));
