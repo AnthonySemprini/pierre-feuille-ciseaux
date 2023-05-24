e15b0a372eddbbfa144ec581bb8e038fe9e451f2
Anthony <semprini.anthony@gmail.com>
2023-05-24 12:20:35 +0200

    recup des elements js

---
+++ app.js
@@ -0,0 +1,11 @@
+// recup element
+let resetBtn = document.getElementById("reset");
+let scorejoueur = document.getElementById("score-joueur");
+let scoreOrdinateur = document.getElementById("score-ordinateur");
+let btnJoueur = [...document.getElementsByClassName("btn-joueur")];
+let opierre = document.getElementById("opierre");
+let ofeuille = document.getElementById("ofeuille");
+let ociseaux = document.getElementById("ociseaux");
+let message = document.getElementById("message");
+let nextBtn = document.getElementById("next");
+
